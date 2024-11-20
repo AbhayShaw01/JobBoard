cbd04487786c12e23e3dd498745ae23ddd45c90f
AbhayShaw01 <abhayshaw0301@gmail.com>
2024-11-20 23:30:18 +0530

    feat: admin authentication

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,5 +1,19 @@
 
 import {z} from "zod";
+import { jobTypes } from "./job-types";
+// const requiredString = z.string().min(1,"Required");
+
+
+// export const createJobSchema = z.object({
+//     title:requiredString.max(100),
+//     type:requiredString.refine(
+//         value => jobTypes.includes(value),
+//         "Invalid job type"
+//     ),
+//     companyName:requiredString.max(100),
+//     companyLogo:
+
+// })
 export const JobFilterSchema = z.object({
     query:z.string().optional(),
     type:z.string().optional(),
